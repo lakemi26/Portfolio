6d9e1ac4c2e5af4c9abb7cb29410713f384f908e
lakemi26 <lakemi@outlook.com.br>
2024-12-18 17:52:39 -0300

    criação de arquivos

---
+++ tailwind.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        pinkzinho: "#f5cfe9",
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
