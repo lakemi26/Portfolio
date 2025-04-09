4454e18399a10e86e8db99425e07d11054bea466
lakemi26 <lakemi@outlook.com.br>
2025-04-09 16:45:04 -0300

    fix: Novo botão de download curriculo

--- components/education/experienceSection.tsx
+++ components/education/experienceSection.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { TextAnimate } from "../ui/text-animate";
 import TabList from "./tabList";
 import { InteractiveHoverButton } from "../ui/interactive-hover-button";
+import Link from "next/link";
 
 const ExperienceSection = () => {
   return (
@@ -12,9 +13,14 @@ const ExperienceSection = () => {
         </TextAnimate>
       </div>
       <TabList />
-      <InteractiveHoverButton className="m-auto">
-        CV Download
-      </InteractiveHoverButton>
+      <Link
+        href="./homeBanner.webp"
+        target="_blank"
+        download
+        className="m-auto border-2 border-pink-500 rounded-full text-pink-500 font-bold p-3 hover:bg-blue-950"
+      >
+        CV Dowload
+      </Link>
     </div>
   );
 };
